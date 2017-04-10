18e4b3efe06fed9318a0036d4f67e82eb1d5773d
tenta1111 <tenta-1111-swan@outlook.com>
2017-04-10 12:20:50 +0900

    add action create and new to users_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :microposts
+  has_secure_password
+  validates :password,presence: true, length: { minimum: 6 }
+
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408145216) do
+ActiveRecord::Schema.define(version: 20170409025036) do
 
   create_table "microposts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -24,8 +24,9 @@ ActiveRecord::Schema.define(version: 20170408145216) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
