4b76d8dc238439f2baa376bea3abcaf51c2ed4b7
tenta1111 <tenta-1111-swan@outlook.com>
2017-05-12 12:00:02 +0900

    for production

--- db/migrate/20170422095152_add_followed_id_and_follower_id_to_relationships.rb
+++ db/migrate/20170422095152_add_followed_id_and_follower_id_to_relationships.rb
@@ -1,7 +1,7 @@
 class AddFollowedIdAndFollowerIdToRelationships < ActiveRecord::Migration[5.0]
   def change
-    add_culumn :follower_id, :relationships, foreign_key: true, index: true
-    add_culumn :followed, :relationships, foreign_key: true, index: true
+    add_culumn :relationships, :follower_id, :integer, foreign_key: true, index: true
+    add_culumn :relationships, :followed_id, :integer, foreign_key: true, index: true
     add_index :relationships, [:follower_id,:followed_id],unique: true
   end
 end
