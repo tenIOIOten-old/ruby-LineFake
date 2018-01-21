3e35d81aa925ac044cdac998c4fc1fcdc41b12ad
tenta1111 <tenta-1111-swan@outlook.com>
2018-01-22 04:06:08 +0900

    edit file secret key

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :branch, 'master'
 set :deploy_to, '/var/www/rails/ruby-LineFake'
 
 # シンボリックリンクをはるファイル。(※後述)
-set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
+set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
 
 # シンボリックリンクをはるフォルダ。(※後述)
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
